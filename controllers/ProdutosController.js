12a92d45f02f842bb228d7369b215514a17d7d3f
Fabio Landin <flandin1990@gmail.com>
2025-03-08 13:27:49 -0300

    Criando Produtos

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -2,14 +2,37 @@
 //permite que crie rotas sem utilizar a variavel app
 const express = require("express");
 const router = express.Router();
+const Produto = require("../produtos/Produto");
+const slugify = require("slugify");
 
 router.get("/produtos", (req, res) => {
     res.send("Rota de Produtos")
 });
 
 //rota para criar novo produto
-router.get("/admin/protudos/new", (req, res) =>{
-    res.send("Rota para criar novo Produto!")
+router.get("/admin/produtos/new", (req, res) =>{
+    res.render("admin/produtos/new");
+})
+
+//salvando produto no banco
+router.post("/produtos/save", (req, res) => {
+    var name = req.body.name;
+    var description = req.body.description;
+    var value = req.body.value;
+    if(name != undefined){
+
+        Produto.create({
+            name: name,
+            description: description,
+            value: value,
+            slug: slugify(name)
+        }).then(() =>{
+            res.redirect("/admin/produtos");
+        })
+    }else{
+        res.redirect("/admin/produtos/new");
+    }
+
 })
 
 module.exports = router;
